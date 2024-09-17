1ab370ceb4bc342cc980c1eb614a297ae58533dd
sadia <smehnaz298@gmail.com>
2024-09-17 12:00:17 +0100

    added my name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+         System.out.println(sayHelloToSomeone("Sadia"));
         System.out.println(sayGoodbye());
     }
 
